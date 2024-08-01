ed7e4b2c035edda2607372a6486fe604558d91cd
kaue_sv <kauesousavieira534@gmail.com>
2024-08-01 15:34:40 -0300

    APIs de atualização dos usuarios

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -1,25 +1,27 @@
 from fastapi import APIRouter
-from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from modelos.banco import MongoDBClient
 from dotenv import load_dotenv
 from datetime import datetime
 from bson import ObjectId
 import os
+from typing import Dict, Any
 
-router = APIRouter()
 
 # --------------
 #   configs
 load_dotenv(dotenv_path='config/config.env')
 uri = os.getenv('URI')
 database_name = os.getenv('DATABASE_NAME')
-api_version = os.getenv('API_VERSION')
+
+router = APIRouter()
 
 # --------------
 #   instancia o banco
 mongo_client = MongoDBClient(str(uri), str(database_name))
 
+# --------------
+#   
 @router.post("/usuarios/", tags=["usuarios"])
 async def post_usuario(nome: str, sobrenome: str):
     """Cria novo usuário"""
@@ -39,9 +41,11 @@ async def post_usuario(nome: str, sobrenome: str):
         user_id = result['_id']
         mensagem = {'id': str(user_id)}
         return JSONResponse(mensagem, status_code=200)
-    
-@router.get("/usuarios/{id}", tags=["usuarios"])
-async def get_usuario(id: str):
+
+# --------------
+#   
+@router.get("/usuarios/id/{id}", tags=["usuarios"])
+async def get_usuario_by_id(id: str):
     """busca um usuário"""
     obj = mongo_client.find_one_document("usuarios", {"_id": ObjectId(str(id))})
 
@@ -59,6 +63,8 @@ async def get_usuario(id: str):
     else:
         return JSONResponse(result, status_code=200)
 
+# --------------
+#   
 @router.get("/usuarios/", tags=["usuarios"])
 async def get_list_usuario():
     """lista os usuários"""
@@ -78,4 +84,54 @@ async def get_list_usuario():
         list_users.append(result)
     return JSONResponse(list_users, status_code=200)
 
+# --------------
+#   
+@router.get("/usuarios/usuario/", tags=["usuarios"])
+async def get_usuario_by_document(document: Dict[str, Any]):
+    """lista os usuários"""
+    obj = mongo_client.find_one_document("usuarios", document)
 
+    result = {
+        "id": str(obj["_id"]),
+        "nome": obj["nome"],
+        "sobrenome": obj["sobrenome"],
+        "criado_em": str(obj["criado_em"]),
+        "atualizado_em": str(obj["atualizado_em"]),
+        "deletado": obj["deletado"]
+    }
+
+    return JSONResponse(result, status_code=200)
+
+# --------------
+#   
+@router.put("/usuarios/{id}", tags=["usuarios"])
+async def put_usuario(id: str, document: Dict[str, Any]):
+    """Atualiza todos os campos"""
+
+    document.update({
+        "atualizado_em": datetime.now(),
+        })
+
+    result = mongo_client.update_document("usuarios", {"_id": ObjectId(str(id))}, document)
+
+    if 'Erro' in result:
+        return JSONResponse(result, status_code=400)
+    else:
+        return JSONResponse(result, status_code=200)
+
+# --------------
+#   Usado para atualizar também o campo de "deletado"
+@router.patch("/usuarios/{id}", tags=["usuarios"])
+async def patch_usuario(id: str, document: Dict[str, Any]):
+    """Atualiza um ou mais campos"""
+
+    document.update({
+        "atualizado_em": datetime.now(),
+        })
+
+    result = mongo_client.update_document("usuarios", {"_id": ObjectId(str(id))}, document)
+
+    if 'Erro' in result:
+        return JSONResponse(result, status_code=400)
+    else:
+        return JSONResponse(result, status_code=200)
