a77e78d05d74d67dee8dd837227b973a7856863e
kaue_sv <kauesousavieira534@gmail.com>
2024-08-26 13:24:15 -0300

    fix: O pytest não encontra o modelos.banco, tentando localizar o problema.

--- tests/test_banco.py
+++ tests/test_banco.py
@@ -1,1 +1,22 @@
 import pytest
+import os
+from dotenv import load_dotenv
+from pymongo.errors import PyMongoError
+from pymongo import MongoClient
+from modelos.banco import MongoDBClient
+
+
+# --------------
+#   configs
+load_dotenv(dotenv_path='config/config.env')
+
+
+def test_connect_to_mongodb():
+    """Test connection to MongoDB"""
+    uri = os.getenv('URI')
+    database_name = os.getenv('TESTE_DATABASE_NAME')
+
+    client = MongoDBClient(uri, database_name)
+    assert client.client is not None
+    assert client.db is not None
+
